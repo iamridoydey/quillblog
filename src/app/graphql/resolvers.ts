a2d43e868a51815d7eff3d797280985d7fbe438e
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-01-15 20:31:24 +0600

    Graphql schema and resolvers

---
+++ src/app/graphql/resolvers.ts
@@ -0,0 +1,174 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { UserServices } from "@/services/userServices";
+import {
+  DeleteUserPayload,
+  GetUserPayload,
+  UpdateUserPayload,
+  UserPayload,
+} from "../interfaces/user";
+import { BlogServices } from "@/services/blogServices";
+import {
+  CreateBlogPayload,
+  DeleteBlogPayload,
+  GetBlogPayload,
+  UpdateBlogPayload,
+} from "../interfaces/blog";
+import {
+  CreatePostPayload,
+  DeletePostPayload,
+  GetPostPayload,
+  UpdatePostPayload,
+} from "../interfaces/post";
+import { PostServices } from "@/services/postServices";
+import {
+  CreateBlogPostPayload,
+  DeleteBlogPostPayload,
+  GetBlogPostPayload,
+  UpdateBlogPostPayload,
+} from "../interfaces/blogPost";
+import { CreateCommentPayload, DeleteCommentPayload, UpdateCommentPayload } from "../interfaces/comment";
+import { BlogPostServices } from "@/services/blogPostServices";
+import { CommentServices } from "@/services/commentServices";
+import { ToggleLikePayload } from "../interfaces/like";
+import { LikeServices } from "@/services/likeServices";
+import { BlogPostTagPayload, BlogTagPayload, PostTagPayload, TagPayload } from "../interfaces/tag";
+import { TagServices } from "@/services/tagServices";
+import { BlogFollowPayload, UserFollowPayload } from "../interfaces/follow";
+import { FollowServices } from "@/services/followServices";
+
+const resolvers = {
+  Query: {
+    // User specific queries
+    getUsers: async () => {
+      return await UserServices.getUsers();
+    },
+    getUser: async (_: any, payload: GetUserPayload) => {
+      return await UserServices.getUser(payload);
+    },
+
+    // Blog specific queries
+    getBlogs: async () => {
+      return await BlogServices.getBlogs();
+    },
+    getBlog: async (_: any, payload: GetBlogPayload) => {
+      return await BlogServices.getBlog(payload);
+    },
+
+    // Post speicif queries
+    getPosts: async () => {
+      return await PostServices.getPosts();
+    },
+    getPost: async (_: any, payload: GetPostPayload) => {
+      return await PostServices.getPost(payload);
+    },
+
+    // BlogPost speicif queries
+    getBlogPosts: async () => {
+      return await BlogPostServices.getBlogPosts();
+    },
+    getBlogPost: async (_: any, payload: GetBlogPostPayload) => {
+      return await BlogPostServices.getBlogPost(payload);
+    },
+  },
+  Mutation: {
+    // User specific mutation
+    createUser: async (_: any, payload: UserPayload) => {
+      return await UserServices.createUser(payload);
+    },
+    updateUser: async (_: any, payload: UpdateUserPayload) => {
+      return await UserServices.updateUser(payload);
+    },
+    deleteUser: async (_: any, payload: DeleteUserPayload) => {
+      return await UserServices.deleteUser(payload);
+    },
+
+    // Blog Specific mutation
+    createBlog: async (_: any, payload: CreateBlogPayload) => {
+      return await BlogServices.createBlog(payload);
+    },
+    updateBlog: async (_: any, payload: UpdateBlogPayload) => {
+      return await BlogServices.updateBlog(payload);
+    },
+    deleteBlog: async (_: any, payload: DeleteBlogPayload) => {
+      return await BlogServices.deleteBlog(payload);
+    },
+
+    // Post Specific mutation
+    createPost: async (_: any, payload: CreatePostPayload) => {
+      return await PostServices.createPost(payload);
+    },
+    updatePost: async (_: any, payload: UpdatePostPayload) => {
+      return await PostServices.updatePost(payload);
+    },
+    deletePost: async (_: any, payload: DeletePostPayload) => {
+      return await PostServices.deletePost(payload);
+    },
+
+    // BlogPost Specific mutation
+    createBlogPost: async (_: any, payload: CreateBlogPostPayload) => {
+      return await BlogPostServices.createBlogPost(payload);
+    },
+    updateBlogPost: async (_: any, payload: UpdateBlogPostPayload) => {
+      return await BlogPostServices.updateBlogPost(payload);
+    },
+    deleteBlogPost: async (_: any, payload: DeleteBlogPostPayload) => {
+      return await BlogPostServices.deleteBlogPost(payload);
+    },
+
+    // Comment specific mutation
+    createComment: async (_: any, payload: CreateCommentPayload) => {
+
+      const result =  await CommentServices.createComment(payload);
+      console.log("Comment, ", result)
+      return result;
+    },
+    updateComment: async (_: any, payload: UpdateCommentPayload) => {
+      return await CommentServices.updateComment(payload);
+    },
+    deleteComment: async (_: any, payload: DeleteCommentPayload) => {
+      return await CommentServices.deleteComment(payload);
+    },
+
+    // Like specific mutation
+    toggleLike: async (_: any, payload: ToggleLikePayload) => {
+      return await LikeServices.toggleLike(payload);
+    },
+
+    // Tag specific mutation
+    createTag: async (_: any, payload: TagPayload) => {
+      return await TagServices.createTag(payload);
+    },
+
+    cleanUpUnusedTags: async () => {
+      return await TagServices.cleanUpUnusedTags();
+    },
+    createPostTag: async (_: any, payload: PostTagPayload) => {
+      return await TagServices.createPostTag(payload);
+    },
+    deletePostTag: async (_: any, payload: PostTagPayload) => {
+      return await TagServices.deletePostTag(payload);
+    },
+    createBlogTag: async (_: any, payload: BlogTagPayload) => {
+      return await TagServices.createBlogTag(payload);
+    },
+    deleteBlogTag: async (_: any, payload: BlogTagPayload) => {
+      return await TagServices.deleteBlogTag(payload);
+    },
+    createBlogPostTag: async (_: any, payload: BlogPostTagPayload) => {
+      return await TagServices.createBlogPostTag(payload);
+    },
+    deleteBlogPostTag: async (_: any, payload: BlogPostTagPayload) => {
+      return await TagServices.deleteBlogPostTag(payload);
+    },
+
+    // Following specific mutation
+    toggleUserFollow: async (_:any, payload: UserFollowPayload)=> {
+      return await FollowServices.toggleUserFollow(payload);
+    },
+    toggleBlogFollow: async (_:any, payload: BlogFollowPayload) =>{
+      return await FollowServices.toggleBlogFollow(payload)
+    }
+  },
+};
+
+export default resolvers;
---
+++ src/app/graphql/schema.graphql.ts
@@ -0,0 +1,212 @@
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  scalar Json
+
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String
+    username: String!
+    email: String!
+    profilePic: String
+    title: String
+    bio: String
+    posts: [Post]
+    blogs: [Blog]
+    blogPosts: [BlogPost]
+    comments: [Comment]
+    following: [User]
+    followers: [User]
+    followedBlogs: [Blog]
+  }
+
+  type Blog {
+    id: ID!
+    userId: ID!
+    shortName: String!
+    title: String!
+    description: String
+    createdAt: String!
+    blogPosts: [BlogPost]
+    tags: [Tag]
+    followers: [User]
+  }
+
+  type BlogPost {
+    id: ID!
+    userId: ID!
+    blogId: ID!
+    title: String!
+    thumbnail: String
+    content: Json!
+    likes: [Like!]
+    comments: [Comment!]
+    createdAt: String!
+    blogPostTags: [BlogPostTag]
+  }
+
+  type Post {
+    id: ID!
+    userId: ID!
+    thumbnail: String
+    content: Json!
+    likes: [Like!]
+    comments: [Comment!]
+    createdAt: String!
+    postTags: [PostTag]
+  }
+
+  type Comment {
+    id: ID!
+    userId: ID!
+    postId: ID
+    blogPostId: ID
+    content: Json!
+    likes: [Like!]
+    isEdited: Boolean
+    createdAt: String!
+  }
+
+  type Tag {
+    id: ID!
+    name: String!
+    blogTags: [BlogTag]
+    postTags: [PostTag]
+    blogPostTags: [BlogPostTag]
+  }
+
+  type BlogTag {
+    blogId: ID!
+    tagId: ID!
+  }
+
+  type BlogPostTag {
+    blogPostId: ID!
+    tagId: ID!
+  }
+
+  type PostTag {
+    postId: ID!
+    tagId: ID!
+  }
+
+  type UserFollow {
+    followerId: ID!
+    followedId: ID!
+    createdAt: String!
+  }
+
+  type UserFollowsBlog {
+    userId: ID!
+    blogId: ID!
+  }
+
+  type Like {
+    id: ID!
+    userId: ID!
+    postId: ID
+    blogPostId: ID
+    commentId: ID
+    createdAt: String!
+  }
+
+  type Query {
+    # User specific queries
+    getUsers: [User]
+    getUser(id: ID, email: String, username: String): User
+
+    # Blog specific queries
+    getBlogs: [Blog]
+    getBlog(id: ID, userId: ID, shortName: String, title: String): Blog
+
+    # Post specific queries
+    getPosts: [Post]
+    getPost(id: ID!): Post
+
+    # BlogPost specific queries
+    getBlogPosts: [BlogPost]
+    getBlogPost(id: ID!): BlogPost
+  }
+
+  type Mutation {
+    createUser(email: String!, password: String!): User
+    updateUser(
+      id: ID!
+      firstName: String
+      lastName: String
+      username: String
+      email: String
+      profilePic: String
+      title: String
+      bio: String
+    ): User
+
+    deleteUser(id: ID!): User
+
+    # Blog specific mutation
+    createBlog(
+      userId: ID!
+      shortName: String!
+      title: String!
+      description: String
+    ): Blog
+    updateBlog(
+      id: ID!
+      shortName: String
+      title: String
+      description: String
+    ): Blog
+    deleteBlog(id: ID!): Blog
+
+    # Post specific mutation
+    createPost(userId: ID!, thumbnail: String, content: Json!): Post
+    updatePost(id: ID!, thumbnail: String, content: Json): Post
+    deletePost(id: ID!): Post
+
+    # BlogPost specific mutation
+    createBlogPost(
+      userId: ID!
+      blogId: ID!
+      title: String!
+      thumbnail: String
+      content: Json!
+    ): BlogPost
+    updateBlogPost(id: ID!, thumbnail: String, content: Json): BlogPost
+    deleteBlogPost(id: ID!): BlogPost
+
+    # Like specific mutation
+    toggleLike(
+      userId: ID!
+      postId: ID
+      blogPostId: ID
+      commentId: ID
+    ): Int
+
+    # Comment specific mutation
+    createComment(
+      userId: ID!
+      postId: ID
+      blogPostId: ID
+      content: Json!
+    ): Comment
+    updateComment(id: ID!, content: String!): Comment
+    deleteComment(id: ID!): Comment
+
+    # Tag specific mutation
+    createTag(name: String!): Tag
+    cleanUpUnusedTags: Boolean
+    createPostTag(postId: ID!, name: String!): PostTag
+    deletePostTag(postId: ID!, name: String!): PostTag
+    createBlogTag(blogId: ID!, name: String!): BlogTag
+    deleteBlogTag(blogId: ID!, name: String!): BlogTag
+    createBlogPostTag(blogPostId: ID!, name: String!): BlogPostTag
+    deleteBlogPostTag(blogPostId: ID!, name: String!): BlogPostTag
+
+    #Follow specific mutation
+    toggleUserFollow(followerId: ID!, followedId: ID!): UserFollow
+    toggleBlogFollow(userId: ID!, blogId: ID!): UserFollowsBlog
+  }
+`;
+
+export default typeDefs;
