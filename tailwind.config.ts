5f6bc1c8bf98c974dc43bb500dbbb4643fa0135d
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-01-16 13:26:58 +0600

    Added Loto and Merriweather fonts to globalcss and tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        merriweather: ["Merriweather", "serif"],
+      },
     },
   },
   plugins: [],
