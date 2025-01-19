dcf17c8ae9b3f5641279d857901bc9682bfc246e
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-01-19 23:52:42 +0600

    Homepage version->1

--- src/app/[page]/home/page.tsx
+++ src/app/[page]/home/page.tsx
@@ -1,35 +1,87 @@
 import PostEditor from "@/components/ui/editor/PostEditor";
 import MiniNav from "../../../components/ui/mininav/MiniNav";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import { MiniNavPayload } from "../../../components/ui/mininav/interfaces";
-import FollowingUser from "@/components/home_components/FollowingUser";
-import FollowingBlogs from "@/components/home_components/FollowingBlogs";
+import {
+  BlogToFollowPayload,
+  TrendingTagsPayload,
+  UserToFollowPayload,
+} from "@/components/ui/sidebar/interfaces";
+import TrendingTags from "@/components/ui/sidebar/TrendingTags";
+import UserToFollow from "@/components/ui/sidebar/UserToFollow";
+import BlogToFollow from "@/components/ui/sidebar/BlogToFollow";
 
 export default function Home() {
-  const pathname = usePathname();
-  const [path, setPath] = useState<string | null>(null);
+  // const pathname = usePathname();
+  // const [path, setPath] = useState<string | null>(null);
 
-  useEffect(() => {
-    setPath(pathname.substring(1));
-  }, [pathname]);
+  // useEffect(() => {
+  //   setPath(pathname.substring(1));
+  // }, [pathname]);
 
   const navItems = ["Feeds", "Following User", "Following Blogs"];
 
-  const navComponents: { [key: string]: React.FC } = {
-    followinguser: FollowingUser,
-    followingblogs: FollowingBlogs,
-  };
+  // const navComponents: { [key: string]: React.FC } = {
+  //   followinguser: FollowingUser,
+  //   followingblogs: FollowingBlogs,
+  // };
+
+  // Sidebar data
+  const tags: TrendingTagsPayload[] = [
+    { tag: "javascript", post: 8734 },
+    { tag: "python", post: 4398 },
+    { tag: "java", post: 2567 },
+    { tag: "c++", post: 6721 },
+    { tag: "react", post: 3458 },
+    { tag: "kotlin", post: 9287 },
+    { tag: "nextjs", post: 1834 },
+  ];
+
+  const users: UserToFollowPayload[] = [
+    { image: "👨🏻‍💼", name: "John Doe", username: "johndoe123" },
+    { image: "👩‍💼", name: "Jane Smith", username: "janesmith" },
+    { image: "👨🏻‍💼", name: "Michael Brown", username: "mikebrown" },
+    { image: "👩‍💼", name: "Emily Johnson", username: "emilyj" },
+  ];
+
+
+  const blogs: BlogToFollowPayload[] = [
+    { image: "🅱️", title: "Mastering JavaScript", shortname: "javascript" },
+    { image: "📘", title: "Python Essentials", shortname: "python" },
+    { image: "🔥", title: "React in Action", shortname: "react" },
+    { image: "🚀", title: "Kotlin for Android", shortname: "kotlin" },
+    { image: "🌐", title: "Next.js Basics", shortname: "nextjs" },
+  ];
+
 
   return (
-    <div className="home_container text-white relative">
-      {/* Home dynamic nav */}
-      <MiniNav navItems={navItems} />
+    <div className="flex gap-8">
+      {/* Main Content */}
+      <div className="main_content w-[600px] border-r-[1px] border-zinc-600">
+        <div className="home_container text-white relative">
+          {/* Home dynamic nav */}
+          <MiniNav navItems={navItems} />
 
-      {/* Create Post */}
-      <PostEditor />
+          {/* Create Post */}
+          <PostEditor />
 
-      {/* Active context */}
+          {/* Active context */}
+        </div>
+      </div>
+
+      {/* Right Sidebar */}
+      <div className="right_sidebar w-[300px] flex-shrink-0 font-lato">
+        <div className="sticky top-0 mt-4">
+          <div className="sidebar_items_wrapper flex flex-col gap-6">
+            {/* Trending tags */}
+            <TrendingTags tags={tags} />
+
+            {/* You may follow */}
+            <UserToFollow users={users} />
+
+            {/* Blog you can follow */}
+            <BlogToFollow blogs={blogs}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/[page]/layout.tsx
+++ src/app/[page]/layout.tsx
@@ -5,17 +5,8 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex gap-8">
-      {/* Main Content */}
-      <div className="main_content w-[600px] border-r-[1px] border-zinc-600">
-
-        {children}
-      </div>
-
-      {/* Right Sidebar */}
-      <div className="right_sidebar w-[300px] flex-shrink-0 border">
-        <div className="sticky top-0">right sidebar</div>
-      </div>
-    </div>
+    <section className="main_layout">
+      {children}
+    </section>
   );
 }
